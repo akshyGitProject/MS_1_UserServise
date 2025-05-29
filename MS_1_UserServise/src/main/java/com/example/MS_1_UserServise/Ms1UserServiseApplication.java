8f85676369bca002a0e5c1220aca589da52032bc
akshy-007 <akshaytakale71@gmail.com>
2025-05-29 09:05:47 +0530

    Initial commit

---
+++ MS_1_UserServise/src/main/java/com/example/MS_1_UserServise/Ms1UserServiseApplication.java
@@ -0,0 +1,13 @@
+package com.example.MS_1_UserServise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ms1UserServiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ms1UserServiseApplication.class, args);
+	}
+
+}
---
+++ MS_1_UserServise/src/test/java/com/example/MS_1_UserServise/Ms1UserServiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MS_1_UserServise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ms1UserServiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
